7aa1ee021c759a5bc757b59055b07388f70b4651
nietztein <jackle12533.gmail.com>
2024-08-16 10:39:46 -0500

    boilerplate

---
+++ firebases.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGbmOXv_moEm0WVCh_k4l-Pqkm-QjN1c4",
+  authDomain: "flashcardsaas-22659.firebaseapp.com",
+  projectId: "flashcardsaas-22659",
+  storageBucket: "flashcardsaas-22659.appspot.com",
+  messagingSenderId: "236186228037",
+  appId: "1:236186228037:web:3ecf67a32cf1e9b18eda2c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
